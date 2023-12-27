fbb0d21724e7cc15d92b4d0294f8b6114c0445a6
Mouhsssine Nabil <46906074+cnabil-me@users.noreply.github.com>
2023-12-27 10:36:09 +0100

    fix2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,18 @@
-# install puppet-lint -v 2.5.0
+# 1-install_a_package.pp
 
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+# Define a class for installing Flask
+class install_flask {
+  package { 'python3-pip':
+    ensure => installed,
+  }
+
+  # Install Flask version 2.1.0 using pip3
+  exec { 'install_flask':
+    command => '/usr/bin/pip3 install Flask==2.1.0',
+    path    => ['/usr/bin'],
+    unless  => '/usr/bin/pip3 show Flask | grep Version | grep -q 2.1.0',
+  }
 }
+
+# Apply the class
+include install_flask
