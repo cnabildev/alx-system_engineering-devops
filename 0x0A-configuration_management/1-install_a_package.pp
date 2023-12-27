0897f8573ac9d342723fef7f738c04ebca01d1bc
Mouhsssine Nabil <46906074+cnabil-me@users.noreply.github.com>
2023-12-27 10:39:26 +0100

    dd

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# Install puppet-lint version 2.5.0
-exec { 'install_puppet_lint':
-  command => '/usr/bin/apt-get -y install puppet-lint=2.5.0',
-  path    => ['/usr/bin'],
-  unless  => '/usr/bin/puppet-lint --version | grep -q "2.5.0"',
+# Install flask
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
